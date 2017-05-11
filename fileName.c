930d348c7e6a231d822c217406aa23fde5949ba6
Gaian <gaian@Gaians-MacBook-Pro.local>
2017-05-11 13:45:46 +0530

    adding a filen

---
+++ fileName.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<float.h>
+extern int a,b,c;
+
+int main()
+{
+float value;
+printf("size %lu\n",sizeof(value));
+printf("Min Value %E\n",FLT_MIN);
+printf("Max Value %E\n",FLT_MAX);
+printf("Precision Value %d\n",FLT_DIG);
+
+
+int a,b,c;
+a=10;b=10;
+c=a+b;
+printf("Variable c value is c=%d\n",c);
+
+}
+
